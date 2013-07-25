d427095c42c3c5b404e2488ee8df03f8534acee7
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-07-25 18:37:38 -0400

    hw submission

--- mta.js
+++ mta.js
@@ -9,81 +9,83 @@
 // Track the total journeys taken
 // Calculate total cost of journeys taken at $2.50 per ride.
 
-function Train(name, stations) {
-  this.name = name;
-  this.stations = stations;
-}
+var tripCounter = 0;
+var tripCost = 2.50;
+var wannaRide = true;
 
-Train.prototype.distance = function(board, exit) {
-  board = this.stations.indexOf(board);
-  exit = this.stations.indexOf(exit);
-  return Math.abs(board - exit);
-};
+while (wannaRide === true) {
 
-var lStations = [ "8th", "6th", "Union Square", "3rd", "1st" ];
-var nStations = [ "Times Square", "34th", "28th", "23rd", "Union Square", "8th" ];
-var sixStations = [ "Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place" ];
-var gStations = [ "Greenpoint", "Nassau", "Metropolitan", "Broadway" ];
-var sStations = [ "Greenpoint", "Nassau", "Metropolitan", "Broadway" ];
-
-var lTrain = new Train('The L Train', lStations);
-var nTrain = new Train('The N Train', nStations);
-var sixTrain = new Train('The Six Train', sixStations);
-var gTrain = new Train('The G Train', gStations);
-
-var trains = [lTrain, nTrain, sixTrain, gTrain, sTrain];
-
-var msg = "Which train would you \nlike to get on?" + displayLines();
-var startTrain = prompt(msg);
-
-var trains = [lTrain, nTrain, sixTrain, gTrain];
-
-var msg = "Which train would you \nlike to get on?\n" + displayLines();
-var startTrain = prompt(msg);
-
-function displayLines() {
-  var trainNames = "";
-  for (var i = 0; i < trains.length; i++) {
-    trainNames += trains[i].name + "\n";
+  function Train(name, stations) {
+    this.name = name;
+    this.stations = stations;
   }
 
-  forEach(trains, trainName);
+  Train.prototype.distance = function(board, exit) {
+    board = this.stations.indexOf(board);
+    exit = this.stations.indexOf(exit);
+    return Math.abs(board - exit);
+  };
 
-  return trainNames.trim();
-}
+  var lStations = [ "8th ave", "6th", "Union Square", "3rd", "1st" ];
+  var nStations = [ "Times Square", "34th", "28th", "23rd-bway", "Union Square", "8th st" ];
+  var sixStations = [ "Grand Central", "33rd", "28th-park", "23rd", "Union Square", "Astor Place" ];
+  var gStations = [ "Greenpoint", "Nassau", "Metropolitan", "Broadway" ];
 
+  var lTrain = new Train('The L Train', lStations);
+  var nTrain = new Train('The N Train', nStations);
+  var sixTrain = new Train('The Six Train', sixStations);
+  var gTrain = new Train('The G Train', gStations);
 
-function trainName(train){
-  return train.name + "\n"
-}
+  var trains = [lTrain, nTrain, sixTrain, gTrain];
 
-function forEach(array, func) {
-  for (var i = 0; i < array.length; i++) {
-    func(array[i]);
+  function displayLines() {
+    var trainList = "";
+    _.each(trains, function(train) {trainList += '\n' + train.name;});
+    return trainList;
   }
-}
 
-function map(array, func) {
-  var newArray = [];
-  for (var i = 0; i < array.length; i++) {
-    newArray.push(func(array[i]));
+  var msg1 = "Which train would you like you get on?" + displayLines();
+  var startTrain = prompt(msg1);
+  var startLine = _.find(trains, function(potato) {return startTrain === potato.name;});
+
+  function displayStops() {
+    var stopList = "";
+    _.each(startLine.stations, function(cucumber) {stopList += '\n' + cucumber;});
+    return stopList;
   }
-  return newArray;
-}
 
-function displayStations() {
-  var train = null;
-  for (var j = 0; j < trains.length; j++) {
-    if (trains[j].name === startTrain) {
-      train = trains[j];
+  var msg2 = "Which stop are you at?" + displayStops();
+  var startStop = prompt(msg2);
+
+  var msg3 = "Which train would you like to get to?" + displayLines();
+  var endTrain = prompt(msg3);
+  var endLine = _.find(trains, function(potato) {return endTrain === potato.name;});
+
+  var msg4 = "Which stop are you getting off?" + displayStops();
+  var endStop = prompt(msg2);
+
+  function calculateStops() {
+    if (startTrain === endTrain) {
+      tripCounter += 1;
+      return startLine.distance(startStop, endStop);
+    } else {
+      intersection = _.intersection(startLine, endLine);
+      var firstDistance = startLine.distance(startStop, intersection);
+      var secondDistance = endLine.distance(endStop, intersection);
+      tripCounter += 1;
+      return firstDistance + secondDistance;
     }
   }
-  var trainStations = "";
-  for (var k = 0; k < train.stations.length; k++) {
-    trainStations += train.stations[k] + "\n";
-  }
-  return trainStations.trim();
-}
 
-var msg2 = "Which station would you \nlike to get on?\n" + displayStations();
-var startStation = prompt(msg2);
+  msg5 = "You have " + calculateStops() + " stops for this trip.\n" +
+    "You've taken " + tripCounter + " trip(s)";
+  alert(msg5);
+
+  var answer = prompt("Wanna ride MTA again? yes or no.");
+
+  if (answer === "no"){
+    totalCost = tripCounter * tripCost;
+    alert("Good bye! Your total cost was $" + totalCost + ".");
+    wannaRide = false;
+  }
+}
